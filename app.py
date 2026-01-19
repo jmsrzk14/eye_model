dda28d9cfbb6f3305c8401c54a4740bf4d5dad36
James Frans Rizky Tambunan <139918042+jmsrzk14@users.noreply.github.com>
2026-01-19 11:47:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -270,4 +270,5 @@ def route_predict():
 
 # ------------------ RUN ------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=False)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=False)
